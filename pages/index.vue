86b59e64408539093b7acc816c827fa27df03994
rs9110 <rs9110@g.rit.edu>
2023-02-06 19:11:10 -0500

    Stable version

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <script setup>
-const animal = ref("");
+const topic = ref("");
 const result = ref("");
+
+let primary = ref("#7664E9");
+let secondary = ref("#A1D631");
+let accent1 = ref("#D13939");
+let accent2 = ref("#008080");
+let accent3 = ref("#C93756");
 
 const onSubmit = async () => {
   fetch("/api/generate", {
@@ -8,12 +14,31 @@ const onSubmit = async () => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ animal: animal.value }),
+    body: JSON.stringify({ topic: topic.value }),
   })
     .then((response) => response.json())
     .then((data) => {
+      // console.log("data = ", data.body);
       if (data.body) {
         result.value = data.body.result;
+        let extract = extractHexValues(result.value); //[ "#2A2A2A", "#4F4F4F", "#7F7F7F" ]
+
+        primary.value = extract[0];
+        secondary.value = extract[1];
+        accent1.value = extract[2];
+        accent2.value = extract[3];
+        accent3.value = extract[4];
+
+        // console.log(
+        //   "values ///// ",
+        //   primary,
+        //   secondary,
+        //   accent1,
+        //   accent2,
+        //   accent3
+        // );
+
+        // console.log(result.value); // {"primary": "#FF0000","secondary": "#FFCF00","accent1": "#FFA500"}
       } else {
         console.error("Response from server does not contain a body property");
       }
@@ -23,16 +48,37 @@ const onSubmit = async () => {
       console.error(error);
     });
 };
+
+function extractHexValues(output) {
+  return output.split(", ");
+}
 </script>
 
 <template>
   <div>
-    replace 'animal' with 'topic'
     <form @submit.prevent="onSubmit">
       <label for="topic">Topic:</label>
       <input id="topic" v-model="topic" />
       <button type="submit">Submit</button>
     </form>
-    <p v-if="result">Result: {{ result }}</p>
+    <div v-if="result">
+      <pre>Result: {{ result }}</pre>
+      <hr />
+      <div id="primary-container" :style="{ backgroundColor: primary }">
+        {{ primary }}
+      </div>
+      <div id="secondary-container" :style="{ backgroundColor: secondary }">
+        {{ secondary }}
+      </div>
+      <div id="accent1-container" :style="{ backgroundColor: accent1 }">
+        {{ accent1 }}
+      </div>
+      <div id="accent2-container" :style="{ backgroundColor: accent2 }">
+        {{ accent2 }}
+      </div>
+      <div id="accent3-container" :style="{ backgroundColor: accent3 }">
+        {{ accent3 }}
+      </div>
+    </div>
   </div>
 </template>
--- server/api/generate.js
+++ server/api/generate.js
@@ -19,13 +19,13 @@ export default defineEventHandler(async (event) => {
     }
 
     const body = await readBody(event)
-    const animal = body.animal || ''
-    if (animal.trim().length === 0) {
+    const topic = body.topic || ''
+    if (topic.trim().length === 0) {
         return {
             status: 400,
             body: {
                 error: {
-                    message: 'Please enter a valid animal'
+                    message: 'Please enter a valid topic'
                 }
             }
         }
@@ -34,8 +34,12 @@ export default defineEventHandler(async (event) => {
     try {
         const completion = await openai.createCompletion({
             model: 'text-davinci-003',
-            prompt: generatePrompt(animal),
-            temperature: 0.6
+            prompt: generatePrompt(topic),
+            temperature: 0,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
         })
         return {
             status: 200,
@@ -63,14 +67,6 @@ export default defineEventHandler(async (event) => {
     }
 })
 
-function generatePrompt(animal) {
-    const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase()
-    return `Suggest three names for an animal that is a superhero.
-  
-  Animal: Cat
-  Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-  Animal: Dog
-  Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-  Animal: ${capitalizedAnimal}
-  Names:`
+function generatePrompt(topic) {
+    return `Generate a color palette with 5 values. Only return the hex value, nothing more. Here is a expected sample output:#7664E9, #A1D631, #D13939, #94623D, #4680AB. Base it on this context: ${topic}`
 }
