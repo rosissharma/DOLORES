3e56b128206abdac9cc95564204512edc3f26342
rs9110 <rs9110@g.rit.edu>
2023-02-08 21:37:03 -0500

    create layout and add header

---
+++ components/MainHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid place-items-center p-4">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <MainHeader />
+    <slot />
+  </div>
+</template>
