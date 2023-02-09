a7f45b7bbc05da565846e2bc6366578c319cf250
rs9110 <rs9110@g.rit.edu>
2023-02-08 21:36:34 -0500

    create color grid block component

---
+++ components/GridBlock.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div
+      class="
+        group
+        sm:w-28
+        h-full
+        grid
+        place-content-center
+        transition
+        ease-in-out
+        delay-150
+      "
+      :style="{ backgroundColor: color }"
+    >
+      <button
+        class="opacity-60 invisible group-hover:visible"
+        @click="clipboard(color)"
+      >
+        {{ color }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+    },
+  },
+};
+</script>
