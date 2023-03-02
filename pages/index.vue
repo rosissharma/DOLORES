a28a1e4c81d738f89a7f1c0375bff498863be702
rs9110 <rs9110@g.rit.edu>
2023-03-01 19:46:03 -0500

    title style and results refactor

--- pages/index.vue
+++ pages/index.vue
@@ -124,35 +124,56 @@ function log(currentTopic) {
       </form>
     </div>
 
-    <div>
-      <h1>Triadic</h1>
-      <!-- triadic color block -->
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
-        <!-- primary -->
-        <TriadicBlock :color="primary" :colorType="typePrimary" />
-        <!-- secondary -->
-        <TriadicBlock :color="secondary" :colorType="typeSecondary" />
-        <!-- accent -->
-        <TriadicBlock :color="accent1" :colorType="typeAccent" />
+    <div v-if="!result && !loading">
+      <div>
+        <!-- title -->
+        <div class="relative flex py-5 items-center">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 title">Triadic</span>
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+        <!-- triadic color block -->
+        <div
+          class="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 gap-10"
+        >
+          <!-- primary -->
+          <TriadicBlock :color="primary" :colorType="typePrimary" />
+          <!-- secondary -->
+          <TriadicBlock :color="secondary" :colorType="typeSecondary" />
+          <!-- accent -->
+          <TriadicBlock :color="accent1" :colorType="typeAccent" />
+        </div>
       </div>
-    </div>
 
-    <!-- color grid block -->
-    <div>
-      <h1>Grid</h1>
-      <div class="h-52 flex gap-1 sm:h-80 sm:gap-4 mx-1">
-        <GridBlock :color="primary" />
-        <GridBlock :color="secondary" />
-        <GridBlock :color="accent1" />
-        <GridBlock :color="accent2" />
-        <GridBlock :color="accent3" />
+      <!-- color grid block -->
+      <div class="mt-6">
+        <!-- title -->
+        <div class="relative flex py-5 items-center">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 title">Grid</span>
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+        <div class="h-52 flex gap-1 sm:h-80 sm:gap-4 mx-1">
+          <GridBlock :color="primary" />
+          <GridBlock :color="secondary" />
+          <GridBlock :color="accent1" />
+          <GridBlock :color="accent2" />
+          <GridBlock :color="accent3" />
+        </div>
       </div>
-    </div>
 
-    <!-- gradient block -->
-    <div class="mt-14">
-      <h1>Gradient</h1>
-      <GradientBlock :gradientColors="[primary, secondary, accent1]" />
+      <!-- gradient block -->
+      <div class="my-10">
+        <!-- title -->
+        <div class="relative flex py-5 items-center">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 title">Gradient</span>
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+        <div class="grid place-items-center">
+          <GradientBlock :gradientColors="[primary, secondary, accent1]" />
+        </div>
+      </div>
     </div>
 
     <!-- display while fetching data -->
@@ -165,24 +186,61 @@ function log(currentTopic) {
     </div>
     <!-- display the results -->
     <div v-else-if="result">
-      <pre>Result: {{ result }}</pre>
-      <hr />
-      <h1>Generated Palette</h1>
-      <div id="primary-container" :style="{ backgroundColor: primary }">
-        {{ primary }}
+      <div>
+        <!-- title -->
+        <div class="relative flex py-5 items-center">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 title">Triadic</span>
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+        <!-- triadic color block -->
+        <div
+          class="grid grid-cols-1 place-items-center sm:grid-cols-2 lg:grid-cols-3 gap-10"
+        >
+          <!-- primary -->
+          <TriadicBlock :color="primary" :colorType="typePrimary" />
+          <!-- secondary -->
+          <TriadicBlock :color="secondary" :colorType="typeSecondary" />
+          <!-- accent -->
+          <TriadicBlock :color="accent1" :colorType="typeAccent" />
+        </div>
       </div>
-      <div id="secondary-container" :style="{ backgroundColor: secondary }">
-        {{ secondary }}
+
+      <!-- color grid block -->
+      <div class="mt-6">
+        <!-- title -->
+        <div class="relative flex py-5 items-center">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 title">Grid</span>
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+        <div class="h-52 flex gap-1 sm:h-80 sm:gap-4 mx-1">
+          <GridBlock :color="primary" />
+          <GridBlock :color="secondary" />
+          <GridBlock :color="accent1" />
+          <GridBlock :color="accent2" />
+          <GridBlock :color="accent3" />
+        </div>
       </div>
-      <div id="accent1-container" :style="{ backgroundColor: accent1 }">
-        {{ accent1 }}
-      </div>
-      <div id="accent2-container" :style="{ backgroundColor: accent2 }">
-        {{ accent2 }}
-      </div>
-      <div id="accent3-container" :style="{ backgroundColor: accent3 }">
-        {{ accent3 }}
+
+      <!-- gradient block -->
+      <div class="my-10">
+        <!-- title -->
+        <div class="relative flex py-5 items-center">
+          <div class="flex-grow border-t border-gray-400"></div>
+          <span class="flex-shrink mx-4 text-gray-400 title">Gradient</span>
+          <div class="flex-grow border-t border-gray-400"></div>
+        </div>
+        <div class="grid place-items-center">
+          <GradientBlock :gradientColors="[primary, secondary, accent1]" />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.title {
+  @apply uppercase font-light;
+}
+</style>
