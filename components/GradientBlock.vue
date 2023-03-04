ec84a48434b4e9fd56a3ec6a04aeda0205b90ae8
rs9110 <rs9110@g.rit.edu>
2023-03-04 15:00:37 -0500

    😮‍💨

--- components/GradientBlock.vue
+++ components/GradientBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-[27rem] sm:w-full h-52 rounded-lg shadow-lg relative sm:h-72 lg:h-96"
+    class="w-[23rem] sm:w-full h-52 rounded-lg shadow-lg relative sm:h-72 lg:h-96"
     :style="{ backgroundImage: createBackgroundString }"
   >
     <button
